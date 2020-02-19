3f1dfcc21dfd4a142ba1854f5701f8e2d4543add
jalhagosipo <gpfla757@naver.com>
2020-02-19 21:08:38 +0900

    feat: input from user and print

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 #!/usr/bin/env node
-console.log('Hello CLI');
-console.log('Hello CLI', process.argv);
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,   // process.stdin : 콘솔입력스트림
+  output: process.stdout, // process.stdout : 콘솔출력스트림
+});
+
+// 화면 지움
+console.clear();
+
+const answerCallback = (answer) => {
+  if (answer === 'y') {
+    console.log('감사합니다!');
+    rl.close();
+  } else if (answer === 'n') {
+    console.log('죄송합니다!');
+    rl.close();
+  } else {
+    console.clear();
+    console.log('y 또는 n만 입력하세요.');
+    rl.question('예제가 재미있습니까? (y/n) ', answerCallback);
+  }
+};
+
+// 첫번째인자 : 질문내용, 두번째인자 : 콜백함수는 답변을 매개변수로 가지고 있음
+rl.question('예제가 재미있습니까? (y/n) ', answerCallback);
